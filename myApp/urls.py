5b60ccf08aade83b073ab22a5b3c6c684e10cca5
SaurabhHable <hablesaurabh02@gmail.com>
2023-01-10 16:52:40 +0530

    Login and logout views added in views.py. urls added for login and logout. LoginPage template modified.

--- myApp/urls.py
+++ myApp/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("login/", views.login, name="login"),
+    path("login/", views.loginPage, name="login"),
+    path("logout/", views.logoutUser, name="logout"),
     path("list/", views.list, name="list"),
 ]
--- myApp/views.py
+++ myApp/views.py
@@ -1,21 +1,44 @@
 from django.shortcuts import render, redirect
 from .forms import productRegistration
 from .models import Product, Category
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required(login_url='login')
 def home(request):
     if request.method == "POST":
         form = productRegistration(request.POST, request.FILES)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Product added successfully.')
             return redirect('/list')
     form = productRegistration()
     return render(request,'myApp/home.html', {'form':form})
 
-def login(request):
-    context = {}
-    return render(request,'myApp/login.html', context)
+def loginPage(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+    
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.info(request, 'Username or Password is incorrect.')
+        context = {}
+        return render(request,'myApp/login.html', context)
 
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
+
+@login_required(login_url='login')
 def list(request):
     products = None
     categories = Category.get_all_categories()
